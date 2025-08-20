e3acf6d0a205462cf7495be048f7d340b3703a56
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-08-20 10:47:37 -0600

    made input notes, and made a hello world program.

---
+++ notes_1st/inputs.py
@@ -0,0 +1,12 @@
+# TH 2nd input and output notes
+
+# one option
+color = input("Give me your favorite color: ")
+print("Your favorite color is", color)
+
+# other option
+print("Tell me your second favorite color: ")
+color_two = input()
+print("Your second favorite color is", color_two)
+
+# pick one method, do not change between them
---
+++ practices/Hello_World.py
@@ -0,0 +1,4 @@
+# TH 2nd Hello World Program
+
+name1 = input("What is your name? ")
+print("Hello", name1)
