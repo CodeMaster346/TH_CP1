0f4d1f340baa8ef650588a2110865a4b4b545245
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-08-21 10:09:44 -0600

    Made sesons starter

---
+++ starters/seasons.py
@@ -0,0 +1,5 @@
+# TH 2nd Seasons Program
+
+lastName = input("What is your last name? ")
+favSeason = input("What is your favorite season? ")
+print("Wow", lastName, "san, I like", favSeason, "too!")
