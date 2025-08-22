55aaab8c8bb043b1a994b05250adeeafc67eb455
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-08-22 10:51:26 -0600

    started who are you project

---
+++ projects/who_are_you.py
@@ -0,0 +1,8 @@
+# TH 2nd  Who are you project
+
+userList = []
+print("Hello User, please answer some questions for me")
+name = input("What is your name?")
+age = input("What is your age?")
+fav_color = input("What is your favorite color?")
+print("Nice to meet you", name,",my favorite color is", fav_color, "as well, but I am not", age)
