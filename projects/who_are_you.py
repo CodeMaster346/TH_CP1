4dad4cb057899c00f68c2277534070b43e75c06f
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-08-24 18:42:17 -0600

    added a print command i forgot to add to the adding user option and added a thing so that there will be a separtion between the user info and the rest of the printed lines.

--- projects/who_are_you.py
+++ projects/who_are_you.py
@@ -10,13 +10,18 @@ while True:
         age = input("What is your age? ")
         fav_color = input("What is your favorite color? ")
         sentence = f"Hello {name}, your favorite color is {fav_color}, and you are {age}."
+        print("--------------------------------------------------------------------------")
+        print(sentence)
+        print("--------------------------------------------------------------------------")
         userList.append(name)
         userList.append(sentence)
     elif options == 2:
         user_input = input("What is your name? ")
         for i in range(len(userList)):
             if userList[i] == user_input:
+                print("--------------------------------------------------------------------------")
                 print(userList[i+1])
+                print("--------------------------------------------------------------------------")
     elif options == 3:
         raise SystemExit
     else:
