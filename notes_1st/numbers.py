8bb8eae73e1ff6680e8f78368689e4172846546b
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-08-27 10:50:48 -0600

    integers and floats notes

---
+++ notes_1st/numbers.py
@@ -0,0 +1,16 @@
+# TH 2nd Integers and Floats Notes
+
+# Integer
+age = 91328749328759874329587
+temp = 85
+attention = -8
+spoons = 4
+
+# Float
+pi = 3.14
+pounds = 5.6
+emotional_damage = .99
+patience_level = -0.50
+
+# MATH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+print(8/4+7-(8+2)%4**2)
