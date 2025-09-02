c11b68475fbe0d59d71a5384a648491ac75ef124
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-09-02 10:51:35 -0600

    made strings notes

--- notes_1st/strings.py
+++ notes_1st/strings.py
@@ -1,2 +1,30 @@
 # TH 2nd Strings Notes
 
+
+# example
+sentence = "The quick brown fox jumps over the lazy dog!"
+first_name = 'Xavier'
+month = 'september'
+food = "chocolate"
+last_name = "LaRose"
+
+# if I need to use ' or "
+name = "La'Rose"
+best = '"The return of the king" is the best book ever!'
+
+funny = "The dog jumps into the lake"
+
+# length of string
+length = len(month)
+print("september has", length, "letters.")
+
+# escape character
+print('\tWill said: \n"don\'t die\"')
+
+# concatenate
+full_name = first_name + " " + last_name
+print(full_name)
+
+# slice
+print(last_name[2:6])
+# index
