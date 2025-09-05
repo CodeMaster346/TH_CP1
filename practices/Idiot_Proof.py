ecde224da594ffc9d44f4a0215c838a0706db18d
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-09-04 19:23:35 -0600

    continued idiot proof program

--- practices/Idiot_Proof.py
+++ practices/Idiot_Proof.py
@@ -1,7 +1,15 @@
 #TH 2nd idiot proof
 
 name = input("What is your full name? ").strip().title()
-phone = int(input("Give me your phone number: "))
+phone = input("Give me your phone number: ")
 gpa = input("Give me your GPA: ")
 
-print(f"")
+print("--------------------------------------------------------------------------------------")
+print(f"Name = {name}")
+digits = ''.join(chr for chr in phone if chr.isdigit())
+first_three_numbers = digits[:3]
+second_three_numbers = digits[3:6]
+last_four_numbers = digits[6:10]
+print(f"Phone Number = {first_three_numbers} {second_three_numbers} {last_four_numbers}")
+print(gpa)
+print("--------------------------------------------------------------------------------------")
