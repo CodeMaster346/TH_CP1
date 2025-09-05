0e2fbd6fe8496fb7e07d66a4add3be8cb38a739c
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-09-05 10:51:10 -0600

    continued idiot proof program

--- practices/Idiot_Proof.py
+++ practices/Idiot_Proof.py
@@ -1,15 +1,25 @@
 #TH 2nd idiot proof
-
-name = input("What is your full name? ").strip().title()
-phone = input("Give me your phone number: ")
-gpa = input("Give me your GPA: ")
-
-print("--------------------------------------------------------------------------------------")
-print(f"Name = {name}")
-digits = ''.join(chr for chr in phone if chr.isdigit())
-first_three_numbers = digits[:3]
-second_three_numbers = digits[3:6]
-last_four_numbers = digits[6:10]
-print(f"Phone Number = {first_three_numbers} {second_three_numbers} {last_four_numbers}")
-print(gpa)
-print("--------------------------------------------------------------------------------------")
+        
+while True:
+    first_name = input("What is your first name? ").strip().title()
+    last_name = input("What is your last name? ").strip().title()
+    phone = input("Give me your phone number: ")
+    gpa = float(input("Give me your GPA: "))
+    if gpa > 4 or gpa < 0:
+        print("--------------------------------------")
+        print("Are you stupid? Input a GPA number!")
+        print("--------------------------------------")
+    elif first_name.isalpha() or last_name.isalpha() == False:
+        print("--------------------------------------")
+        print("Are you stupid? Input a name!")
+        print("--------------------------------------")
+    else:
+        print("--------------------------------------------------------------------------------------")
+        print(f"Name = {name}")
+        digits = ''.join(chr for chr in phone if chr.isdigit())
+        first_three_numbers = digits[:3]
+        second_three_numbers = digits[3:6]
+        last_four_numbers = digits[6:10]
+        print(f"Phone Number = {first_three_numbers} {second_three_numbers} {last_four_numbers}")
+        print(round(gpa, 1))
+        print("--------------------------------------------------------------------------------------")
