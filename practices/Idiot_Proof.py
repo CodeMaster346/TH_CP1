c0878dd1ce3fdad195f70c201571abf4d34e629f
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-09-05 18:31:07 -0600

    finished idiot proof project

--- practices/Idiot_Proof.py
+++ practices/Idiot_Proof.py
@@ -1,25 +1,99 @@
 #TH 2nd idiot proof
-        
+
 while True:
     first_name = input("What is your first name? ").strip().title()
     last_name = input("What is your last name? ").strip().title()
     phone = input("Give me your phone number: ")
     gpa = float(input("Give me your GPA: "))
+
     if gpa > 4 or gpa < 0:
         print("--------------------------------------")
         print("Are you stupid? Input a GPA number!")
         print("--------------------------------------")
+        while gpa > 4 or gpa < 0:
+            gpa = float(input("Give me your GPA: "))
+            if gpa > 4 or gpa < 0:
+                    print("--------------------------------------")
+                    print("Are you stupid? Input a GPA number!")
+                    print("--------------------------------------")
+            elif first_name.isalpha() or last_name.isalpha() == False:
+                print("--------------------------------------")
+                print("Are you stupid? Input a name!")
+                print("--------------------------------------")
+                while first_name.isalpha() or last_name.isalpha() == False:
+                    name = input("Give me your name: ")
+                    if first_name.isalpha() or last_name.isalpha() == False:
+                        print("--------------------------------------")
+                        print("Are you stupid? Input a valid name!")
+                        print("--------------------------------------")
+                    else :
+                        print("--------------------------------------------------------------------------------------")
+                        print(f"Name = {first_name} {last_name}")
+                        digits = ''.join(chr for chr in phone if chr.isdigit())
+                        first_three_numbers = digits[:3]
+                        second_three_numbers = digits[3:6]
+                        last_four_numbers = digits[6:10]
+                        print(f"Phone Number = {first_three_numbers} {second_three_numbers} {last_four_numbers}")
+                        print(f"GPA = {round(gpa, 1)}")
+                        print("--------------------------------------------------------------------------------------")
+            else :
+                print("--------------------------------------------------------------------------------------")
+                print(f"Name = {first_name} {last_name}")
+                digits = ''.join(chr for chr in phone if chr.isdigit())
+                first_three_numbers = digits[:3]
+                second_three_numbers = digits[3:6]
+                last_four_numbers = digits[6:10]
+                print(f"Phone Number = {first_three_numbers} {second_three_numbers} {last_four_numbers}")
+                print(f"GPA = {round(gpa, 1)}")
+                print("--------------------------------------------------------------------------------------")
+
     elif first_name.isalpha() or last_name.isalpha() == False:
         print("--------------------------------------")
         print("Are you stupid? Input a name!")
         print("--------------------------------------")
+        while first_name.isalpha() or last_name.isalpha() == False:
+            name = input("Give me your name: ")
+            if first_name.isalpha() or last_name.isalpha() == False:
+                print("--------------------------------------")
+                print("Are you stupid? Input a valid name!")
+                print("--------------------------------------")
+            elif gpa > 4 or gpa < 0:
+                print("--------------------------------------")
+                print("Are you stupid? Input a GPA number!")
+                print("--------------------------------------")
+                while gpa > 4 or gpa < 0:
+                    gpa = float(input("Give me your GPA: "))
+                    if gpa > 4 or gpa < 0:
+                        print("--------------------------------------")
+                        print("Are you stupid? Input a GPA number!")
+                        print("--------------------------------------")
+                    else :
+                        print("--------------------------------------------------------------------------------------")
+                        print(f"Name = {first_name} {last_name}")
+                        digits = ''.join(chr for chr in phone if chr.isdigit())
+                        first_three_numbers = digits[:3]
+                        second_three_numbers = digits[3:6]
+                        last_four_numbers = digits[6:10]
+                        print(f"Phone Number = {first_three_numbers} {second_three_numbers} {last_four_numbers}")
+                        print(f"GPA = {round(gpa, 1)}")
+                        print("--------------------------------------------------------------------------------------")
+            else :
+                print("--------------------------------------------------------------------------------------")
+                print(f"Name = {first_name} {last_name}")
+                digits = ''.join(chr for chr in phone if chr.isdigit())
+                first_three_numbers = digits[:3]
+                second_three_numbers = digits[3:6]
+                last_four_numbers = digits[6:10]
+                print(f"Phone Number = {first_three_numbers} {second_three_numbers} {last_four_numbers}")
+                print(f"GPA = {round(gpa, 1)}")
+                print("--------------------------------------------------------------------------------------")
     else:
         print("--------------------------------------------------------------------------------------")
-        print(f"Name = {name}")
+        print(f"Name = {first_name} {last_name}")
         digits = ''.join(chr for chr in phone if chr.isdigit())
         first_three_numbers = digits[:3]
         second_three_numbers = digits[3:6]
         last_four_numbers = digits[6:10]
         print(f"Phone Number = {first_three_numbers} {second_three_numbers} {last_four_numbers}")
-        print(round(gpa, 1))
+        print(f"GPA = {round(gpa, 1)}")
         print("--------------------------------------------------------------------------------------")
