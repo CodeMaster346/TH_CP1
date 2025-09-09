8e09e0f8d499dfdf91513d58866cd04731770beb
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-09-09 16:26:47 +0000

    added print to test.py

--- notes_1st/test.py
+++ notes_1st/test.py
@@ -1,4 +1,5 @@
 print("test")
 print("practice sync and homework")
+print("I added this to codespaces. That place where I can do homework.")
 #print() <= Everything inside the parenthesis appears inside the terminal
 #this is a comment
