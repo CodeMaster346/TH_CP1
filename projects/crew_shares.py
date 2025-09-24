f1aa5dda785c9f80434995c52e05c1318589f3ff
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-09-23 19:10:00 -0600

    syncing the change made on the crew shares project

--- projects/crew_shares.py
+++ projects/crew_shares.py
@@ -3,6 +3,11 @@
 cash = float(input("How much money did the crew collect(dont put a dollar sign)? "))
 members = int(input("How many crew members are there (not including captian and first mate)? "))
 one_share = cash / (members + 10)
-print(f"The captian gets: ${one_share * 7:.2f}")
-print(f"The first mate gets: ${one_share * 3:.2f}")
-print(f"The crew members indivdually each get: ${one_share - 500:.2f}")
+if one_share - 500 < 0:
+    print(f"The captian gets: ${one_share * 7:.2f}")
+    print(f"The first mate gets: ${one_share * 3:.2f}")
+    print("The crew members indivdually each get: $0")
+else:
+    print(f"The captian gets: ${one_share * 7:.2f}")
+    print(f"The first mate gets: ${one_share * 3:.2f}")
+    print(f"The crew members indivdually each get: {one_share - 500:.2f}")
