4600f7b4fca02a87266cbd6fef4a13b9a5454cb2
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-09-25 10:51:03 -0600

    started debugging notes

---
+++ notes_1st/debugging.py
@@ -0,0 +1,12 @@
+# TH 2nd debugging code
+
+#Syntax errrors - You wrote your code incorrectly (incorrect "Grammar")
+    #print"Hello"
+#Fix by reading where the problem is and fix it
+
+#Logic Error -  the computer gets the wrong steps
+num = "10"
+num_two = "3"
+print(num+num_two)
+
+#Runtime Errors - 
