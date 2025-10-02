deb3944051f8b0fe22b8fa05e4d4644e7ded8129
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-10-02 10:51:33 -0600

    continued my multiplication table practice

--- practices/multiplication_table.py
+++ practices/multiplication_table.py
@@ -1,5 +1,8 @@
-mult_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+mult_nums = [1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 iteration = 1
 
+print("  X  |   1   |   2   |   3     4     5     6     7     8     9    10    11    12\n")
 for mult_num in mult_nums:
+    print(f"{mult_nums[0] * iteration:^5}|{mult_nums[1] * iteration:^5}|{mult_nums[2] * iteration:^5}|{mult_nums[3] * iteration:^5}|{mult_nums[4] * iteration:^5}|{mult_nums[5] * iteration:^5}|{mult_nums[6] * iteration:^5}|{mult_nums[7] * iteration:^5}|{mult_nums[8] * iteration:^5}|{mult_nums[9] * iteration:^5}|{mult_nums[10] * iteration:^5}|{mult_nums[11] * iteration:^5}|{mult_nums[12] * iteration:^5}\n")
+    iteration = iteration + 1
