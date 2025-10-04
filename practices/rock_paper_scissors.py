1b3ce701fd894a369db13155a5ab03971f6b7d6a
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-10-04 12:55:32 -0600

    updated rock paper scissors code a bit

--- practices/rock_paper_scissors.py
+++ practices/rock_paper_scissors.py
@@ -5,7 +5,7 @@ bot_desision = random.randint(1,3)
 
 
 
-user_decision = int(input("1. rock\n2.Paper\n3. Scissors? "))
+user_decision = int(input("1. rock\n2. Paper\n3. Scissors\nInput the number of the option you want to choose: "))
 
 if user_decision == 1 and bot_desision == 2:
     print("placeholder for player lose")
@@ -16,6 +16,12 @@ elif user_decision == 1 and bot_desision == 3:
 elif user_decision == 2 and bot_desision == 2:
     print("placeholder for a draw")
 elif user_decision == 2 and bot_desision == 1:
-    print("placeholder for a draw")
-elif user_decision == 2 and bot_desision == 3:
     print("placeholder for a player win")
+elif user_decision == 2 and bot_desision == 3:
+    print("placeholder for a player lose")
+elif user_decision == 3 and bot_desision == 2:
+    print("placeholder for a player win")
+elif user_decision == 3 and bot_desision == 1:
+    print("placeholder for a player lose")
+elif user_decision == 3 and bot_desision == 3:
+    print("placeholder for a draw")
