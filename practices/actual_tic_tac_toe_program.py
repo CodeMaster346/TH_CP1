22b04d7d0d4d18b30fdc451cd26137a8620eb7fc
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-10-10 16:12:26 -0600

    continued tic tac toe pseudocode and program

--- practices/actual_tic_tac_toe_program.py
+++ practices/actual_tic_tac_toe_program.py
@@ -1,13 +1,18 @@
-rowOne = [1, 2, 3]
-rowTwo = [4, 5, 6]
-rowThree = [7, 8, 9]
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 win = 0
 
 while win == 0:
-    print(f"{rowOne[0]:^5}|{rowOne[1]:^5}|{rowOne[2]:^5}\n ---------------")
-    print(f"{rowTwo[0]:^5}|{rowTwo[1]:^5}|{rowTwo[2]:^5}\n ---------------")
-    print(f"{rowThree[0]:^5}|{rowThree[1]:^5}|{rowThree[2]:^5}")
-    first_player_choice = input("Input the number of the space you want to claim")
-    first_player_choice.isdigit()
-    if
+    print(f"{board[0]:^5}|{board[1]:^5}|{board[2]:^5}\n ---------------")
+    print(f"{board[3]:^5}|{board[4]:^5}|{board[5]:^5}\n ---------------")
+    print(f"{board[6]:^5}|{board[7]:^5}|{board[8]:^5}")
+    first_player_choice = int(input("\nInput the number of the space you want to claim: "))
+    
+    if first_player_choice not in board:
+        while first_player_choice not in board:
+            print(f"{board[0]:^5}|{board[1]:^5}|{board[2]:^5}\n ---------------")
+            print(f"{board[3]:^5}|{board[4]:^5}|{board[5]:^5}\n ---------------")
+            print(f"{board[6]:^5}|{board[7]:^5}|{board[8]:^5}")
+            first_player_choice = int(input("\nInput the number of the space you want to claim: "))
+    else:
+        print("hello")
