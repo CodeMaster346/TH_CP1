75018b2d94aa92c8e9fb9ce093efb4058dfe61df
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-10-22 10:51:54 -0600

    continued ceaser cipher code

--- practices/ceaser_cipher.py
+++ practices/ceaser_cipher.py
@@ -1,10 +1,48 @@
 #TH 2nd Ceaser Cipher Practice
 
+#CONTAINER that holds the different characters set to variable new_letter
+
+#DEFINE the FUNCTION for encoding which needs the VARIABLE sentence
+#   FOR every character in the VARIABLE sentence
+#       IF charater IS a letter THEN
+#           SET new_letter_value AS the ASCII version of that character PLUS the shift number
+#           SET new_letter AS the LETTER version of new_letter_value
+#           ADD new_letter to CONTAINER
+#       ELSE
+#           ADD character to CONTAINER
+
+#DEFINE the FUNCTION for decoding which needs the VARIABLE sentence
+#   FOR every character in the VARIABLE sentence
+#       IF charater IS a letter THEN
+#           SET new_letter_value AS the ASCII version of that character MINUS the shift number
+#           SET new_letter AS the LETTER version of new_letter_value
+#           ADD new_letter to CONTAINER
+#       ELSE
+#           ADD character to CONTAINER
+
+#PROMPT user to enter the sentence they want to encode/decode
+#SET sentence AS the INPUT the user gives
+#PROMPT user to enter the option to encode or decode the sentence
+#SET encode_or_decode AS the INPUT the user gives
+#IF encode_or_decode IS SET AS 1 THEN
+#   PROMPT user to give what number they want to shift by 
+
+newSentenceList = []
+
+
 def encode(sentence):
     for char in sentence:
-        new_letter_value = ord(f"{char}") + shift_number
-        new_letter = chr(new_letter_value)
+        if char.isalpha():
+            new_letter_value = ord(f"{char}") + shift_number
+            new_letter = chr(new_letter_value)
+            newSentenceList.append(new_letter)
+        else:
+            newSentenceList.append(char)
 
-while True:
-    sentence = input("Give me the sentence or phrase you want to encode/decode: ")
-    encode_or_decode = int(input("1. Encode\n2. Decode\nGive me the number of the option you want to choose: "))
+sentence = input("Give me the sentence or phrase you want to encode/decode: ")
+encode_or_decode = int(input("1. Encode\n2. Decode\nGive me the number of the option you want to choose: "))
+if encode_or_decode == 1:
+    shift_number = int(input("What number do you want to shift the sentence by(Max number is 25)? "))
+    encode(sentence)
+    new_sentence = ''.join(newSentenceList)
+    print(new_sentence)
