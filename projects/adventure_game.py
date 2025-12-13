e4f6a720bcd95ce89d785548a1bdfd1b31aeda9e
CodeMaster346 <tyler.hurst@ucas-edu.net>
2025-12-13 02:09:16 +0000

    fully debugged and finished code

--- projects/adventure_game.py
+++ projects/adventure_game.py
@@ -4,7 +4,7 @@ import random
 
 escape = False
 wins = 1
-check_win = False
+check_win = 2
 
 powerUpOptions = ["Health Potions", "Strength Potions", "Extra Armor", "Sword", "Bow and Arrow", "Wizard staff", "Firebending Potion", "Speed Potion"]
 statDictionary = {"Player Health" : 30, "Player Damage" : 10, "Guard Health" : 40, "Guard Damage" : 8, "Witch Health" : 35, "Witch Damage" : 10}
@@ -25,11 +25,12 @@ def restart():
             restarting_choice = input("Do you wish to restart(Y/N): ")
             restarting_choice.upper()
 
-def win_or_lose(check_win):
-    if check_win == True:
+def win_or_lose(check_win, wins):
+    if check_win == 1:
         if wins == 1:
             powerUps.append("Key")
             print("You got the key.")
+        time.sleep(2)
         print("You won and got 3 power-ups!")
         power_up_1 = powerUpOptions[random.randint(1, 8) - 1]
         power_up_2 = powerUpOptions[random.randint(1, 8) - 1]
@@ -40,6 +41,8 @@ def win_or_lose(check_win):
     else:
         print("You Lost")
         restart()
+        return
+
 
 def user_power_up_choice(escape):
     iteration = 0
@@ -79,7 +82,7 @@ def user_power_up_choice(escape):
         return escape
         
 
-def battle(monster, check_win):
+def battle(monster, check_win, wins):
     original_monster_health = statDictionary[f"{monster} Health"]
     original_player_health = statDictionary[f"Player Health"]
     print(f"You Are battling the {monster}\nIts stats are:\nHealth = {statDictionary[f"{monster} Health"]}\nDamage = {statDictionary[f"{monster} Damage"]}")
@@ -154,6 +157,7 @@ def battle(monster, check_win):
                     time.sleep(2)
                     print("You didnt escape and took a fatal blow.")
                     restart()
+                    return
             time.sleep(3)
         print(f"It is the {monster}'s turn!")
         hit = random.randint(1, 2)
@@ -162,13 +166,13 @@ def battle(monster, check_win):
             statDictionary["Player Health"] -= statDictionary[f"{monster} Damage"]
         else:
             print(f"The {monster} missed")
-    if statDictionary["Player Health"] <= 0:
-        check_win = False
+    if statDictionary[f"{monster} Health"] <= 0:
+        check_win = 1
     else:
-        check_win == True
+        check_win == 2
     statDictionary["Player Health"] = original_player_health
     statDictionary[f"{monster} Health"] = original_monster_health
-    win_or_lose(check_win)
+    win_or_lose(check_win, wins)
 
 def main(check_win):
     while True:
@@ -189,7 +193,8 @@ def main(check_win):
                 steal_keys = steal_keys.upper()
                 if steal_keys == "Y":
                     for i in range(1):
-                        battle("Guard", check_win)
+                        wins = 1
+                        battle("Guard", check_win, wins)
                     statDictionary["Player Health"] += 10
                 else:
                     wins = 2
@@ -199,7 +204,7 @@ def main(check_win):
                     if bright_or_dark == 1:
                         print("You got caught by 5 members of the Royal Army")
                         for i in range(5):
-                            battle("Guard", check_win)
+                            battle("Guard", check_win, wins = 2)
                         print("You escaped using the risky ending! Congrats!")
                         if endings[0] == "_____________":
                             endings[0] = "Risky"
@@ -226,63 +231,65 @@ def main(check_win):
                                 print("You went in to the tavern and found a power-up in the trash.")
                                 power_up = powerUpOptions[random.randint(1, 8) - 1]
                                 powerUps.append(power_up)
-                            else:
+                            time.sleep(2)
+                            print("You came across a house and went in.")
+                            time.sleep(1)
+                            stay = input("Do you want to stay and see what the amazing smell is(Y/N)?")
+                            stay = stay.upper()
+                            if stay == "Y":
                                 time.sleep(2)
-                                print("You came across a house and went in.")
+                                print("You stayed and ran into a witch that tries to cook you.")
                                 time.sleep(1)
-                                stay = input("Do you want to stay and see what the amazing smell is(Y/N)?")
-                                stay = stay.upper()
-                                if stay == "Y":
-                                    time.sleep(2)
-                                    print("You stayed and ran into a witch that tries to cook you.")
-                                    time.sleep(1)
-                                    stab = int(input("1. Pour boiling water on the witch\n2. Stab the witch with a knife\nInput the number for the option you wish to choose"))
+                                stab = int(input("1. Pour boiling water on the witch\n2. Stab the witch with a knife\nInput the number for the option you wish to choose"))
 
-                                    if stab == 1:
-                                        time.sleep(2)
-                                        print("The witch melted and you ran out the back door.")
-                                    else:
-                                        time.sleep(2)
-                                        print("Stabing the witch just made the witch mad at you.")
-                                        battle("Witch", check_win)
-                                time.sleep(2)
-                                print("You escaped and got to the gate, but you ran into a guard")
-                                time.sleep(2)
-                                battle("Guard", check_win)
-                                if "Key" in powerUps:
+                                if stab == 1:
                                     time.sleep(2)
-                                    print("You had the key and escaped through the gate using the battle ready ending! Congrats!")
-                                    if endings[0] == "_____________":
-                                        endings[0] = "Battle Ready"
-                                    elif endings[1] == "_____________":
-                                        endings[1] = "Battle Ready"
-                                    elif endings[2] == "_____________":
-                                        endings[2] = "Battle Ready"
-                                    else:
-                                        endings[3] = "Battle Ready"
+                                    print("The witch melted and you ran out the back door.")
                                 else:
                                     time.sleep(2)
-                                    print("You climbed the wall and ran into your parents who ordered 2 guards to capture you.")
-                                    for i in range(2):
-                                        battle("Guard", check_win)
-                                    time.sleep(2)
-                                    print("You escaped using the unprepared ending. Congrats I guess.")
-                                    if endings[0] == "_____________":
-                                        endings[0] = "Unprepared"
-                                    elif endings[1] == "_____________":
-                                        endings[1] = "Unprepared"
-                                    elif endings[2] == "_____________":
-                                        endings[2] = "Unprepared"
-                                    else:
-                                        endings[3] = "Unprepared"
+                                    print("Stabing the witch just made the witch mad at you.")
+                                    battle("Witch", check_win, wins = 2)
+                            time.sleep(2)
+                            print("You escaped and got to the gate, but you ran into a guard")
+                            time.sleep(2)
+                            battle("Guard", check_win, wins = 2)
+                            if "Key" in powerUps:
+                                time.sleep(2)
+                                print("You had the key and escaped through the gate using the battle ready ending! Congrats!")
+                                if endings[0] == "_____________":
+                                    endings[0] = "Battle Ready"
+                                elif endings[1] == "_____________":
+                                    endings[1] = "Battle Ready"
+                                elif endings[2] == "_____________":
+                                    endings[2] = "Battle Ready"
+                                else:
+                                    endings[3] = "Battle Ready"
+                            else:
+                                time.sleep(2)
+                                print("You climbed the wall and ran into your parents who ordered 2 guards to capture you.")
+                                for i in range(2):
+                                    battle("Guard", check_win, wins = 2)
+                                time.sleep(2)
+                                print("You escaped using the unprepared ending. Congrats I guess.")
+                                if endings[0] == "_____________":
+                                    endings[0] = "Unprepared"
+                                elif endings[1] == "_____________":
+                                    endings[1] = "Unprepared"
+                                elif endings[2] == "_____________":
+                                    endings[2] = "Unprepared"
+                                else:
+                                    endings[3] = "Unprepared"
             if escape_choice == 3:
                 time.sleep(2)
                 print("You woke up you siblings and you decided to take the family plane to get out")
                 statDictionary["Player Health"] = statDictionary["Player Health"] * 3
+                original_player_health = statDictionary["Player Health"]
                 time.sleep(2)
                 print("You ran into 10 air force pilot guards.")
+                wins = 2
                 for i in range(10):
-                    battle("Guard", check_win)
+                    battle("Guard", check_win, wins = 2)
+                    statDictionary["Player Health"] = original_player_health
                 time.sleep(2)
                 print("You escaped using the plane ending! Congrats!")
                 if endings[0] == "_____________":
@@ -295,5 +302,11 @@ def main(check_win):
                     endings[3] = "Plane"
             else:
                 print("You stupid, You entered an invalid number")
+        elif play_or_exit == 2:
+            raise SystemExit
+        elif play_or_exit == 3:
+            print(endings)
+        else:
+            print("You stupid")
 
 main(check_win)
